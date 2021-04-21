4c0db2e29e40eb287fe11276eb502380b5fba2da
L3p1989 <phogan110@live.com>
2021-04-21 02:00:29 -0700

    set up PeopleCard

---
+++ src/components/PeopleCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+
+const PeopleCard = props => {
+
+    return (
+        <div className="col-6 text-center">
+            <div className="card shadow my-2">
+                <h5 className="card-title">{props.name}</h5>
+                <p className="card-text">{props.gender}</p>
+                <Link to={`/people/${props.id}`} className="btn btn-primary w-50 ">more info</Link>
+            </div>
+        </div>
+    );
+};
+
+export default PeopleCard;
