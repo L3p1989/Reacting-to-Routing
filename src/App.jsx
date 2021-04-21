4884462f463ffa46258c2c2f812d6b4ddb6c5c22
L3p1989 <phogan110@live.com>
2021-04-21 00:14:37 -0700

    imported Films, set up Films route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 import Home from './pages/Home';
+import Films from './pages/Films';
 
 const App = () => {
     return (
@@ -10,6 +11,9 @@ const App = () => {
             <Switch>
                 <Route exact path="/">
                     <Home />
+                </Route>
+                <Route exact path="/Films">
+                    <Films />
                 </Route>
             </Switch>
         </Router>
