bbbcb3c9b3bbd50e348c81fb41985297c11fd92f
L3p1989 <phogan110@live.com>
2021-04-21 02:04:39 -0700

    added style to btn

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -7,7 +7,7 @@ const PeopleCard = props => {
             <div className="card shadow my-2">
                 <h5 className="card-title">{props.name}</h5>
                 <p className="card-text">{props.gender}</p>
-                <Link to={`/people/${props.id}`} className="btn btn-primary w-50 ">more info</Link>
+                <Link to={`/people/${props.id}`} className="btn btn-primary w-50" style={{ marginLeft: "25%" }}>more info</Link>
             </div>
         </div>
     );
