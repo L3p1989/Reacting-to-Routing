6385faad5d5c6fc835483efdbcb7dd4f3e1db3cc
L3p1989 <phogan110@live.com>
2021-04-21 16:46:06 -0700

    corrected card styling

--- src/components/PeopleCard.jsx
+++ src/components/PeopleCard.jsx
@@ -5,9 +5,12 @@ const PeopleCard = props => {
     return (
         <div className="col-6 text-center">
             <div className="card shadow my-2">
-                <h5 className="card-title">{props.name}</h5>
-                <p className="card-text">{props.gender}</p>
-                <Link to={`/people/${props.id}`} className="btn btn-primary w-50" style={{ marginLeft: "25%" }}>more info</Link>
+                <div className="card-body">
+                    <h5 className="card-title">{props.name}</h5>
+                    <p className="card-text">{props.gender}</p>
+                    <Link to={`/people/${props.id}`} className="btn btn-primary">more info</Link>
+                </div>
+
             </div>
         </div>
     );
