63a5f0ed877add5bfdb8daa78f9090a5ffab396a
L3p1989 <phogan110@live.com>
2021-04-20 23:44:02 -0700

    set up NavBar styled classes and text

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,13 @@
 const NavBar = () => {
     return (
         <>
-            <nav className="navbar navbar-dark bg-dark">
-                Test
+            <nav className="navbar navbar-light sticky-top" style={{ backgroundColor: "#e3f2fd" }}>
+                <img src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="" className="navbar-brand" style={{ height: "3em" }} />
+                <ul className="navbar mr-auto">
+                    <div className="nav-link">Home</div>
+                    <div className="nav-link">Films</div>
+                    <div className="nav-link">People</div>
+                </ul>
             </nav>
         </>
     );
