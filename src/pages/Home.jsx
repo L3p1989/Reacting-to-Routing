a0d772379c53abdaf29d01c555c22cfd82f200fa
L3p1989 <phogan110@live.com>
2021-04-20 23:57:23 -0700

    set up home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+const Home = () => {
+    return (
+        <div className="container">
+            <div className="row justify-content-center mt-2">
+                <h1>Reacting to Studio Ghibli API</h1>
+            </div>
+            <div className="row justify-content-center mt-5">
+                <p className="font-weight-bold">The Studio Ghibli API catalogs the people, places, and things found in the worlds of Ghibli. It was created to help users discover resources, consume them via HTTP requests, and interact with them in whatever way makes sense. Navigation can be found on the top navbar.</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
