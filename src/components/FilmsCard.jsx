19dee08169d8428bf8dad4b1df00bac23653832c
L3p1989 <phogan110@live.com>
2021-04-21 01:03:47 -0700

    styled footer

--- src/components/FilmsCard.jsx
+++ src/components/FilmsCard.jsx
@@ -7,8 +7,8 @@ const FilmsCard = props => {
                 <div className="card-body text-center">
                     <h5 className="card-title"><u>{props.title}</u></h5>
                     <p className="card-text">{props.description}</p>
-                    {props.rt_score > 70 ? <footer className="blockquote-footer text-success">{props.rt_score}</footer> : <footer className="blockquote-footer text-danger">{props.rt_score}</footer>}
-                    <Link to={`/films/${props.id}`} className="btn btn-primary">More Info</Link>
+                    {props.rt_score > 70 ? <footer className="blockquote-footer text-success font-weight-bold">{props.rt_score}</footer> : <footer className="blockquote-footer text-danger font-weight-bold">{props.rt_score}</footer>}
+                    <Link to={`/films/${props.id}`} className="btn btn-primary mt-2">More Info</Link>
                 </div>
             </div>
         </div>
