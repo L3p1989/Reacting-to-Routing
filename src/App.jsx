15ddae162a053438b51ac154e5274b7fdd918c69
L3p1989 <phogan110@live.com>
2021-04-20 23:57:48 -0700

    imported Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
+import Home from './pages/Home';
 
 const App = () => {
     return (
         <Router>
             <NavBar />
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+            </Switch>
         </Router>
     );
 };
