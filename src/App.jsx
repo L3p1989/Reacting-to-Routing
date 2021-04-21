ceb3fa585ce7456698c788e656cefb9415b5cd01
L3p1989 <phogan110@live.com>
2021-04-21 01:36:28 -0700

    imported People page, set up Route for People

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Films from './pages/Films';
 import FilmDetail from './pages/FilmDetail';
+import People from './pages/People';
 
 const App = () => {
     return (
@@ -18,6 +19,9 @@ const App = () => {
                 </Route>
                 <Route exact path="/films/:filmid">
                     <FilmDetail />
+                </Route>
+                <Route exact path="/people">
+                    <People />
                 </Route>
             </Switch>
         </Router>
