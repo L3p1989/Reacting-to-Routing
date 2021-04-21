e874ecf0b0e92b0f7d6330b6ded70a8bd1a5aae0
L3p1989 <phogan110@live.com>
2021-04-20 22:47:46 -0700

    set up App

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const App = () => {
+    return (
+        <Router>
+            <h1>this is App</h1>
+        </Router>
+    )
+}
