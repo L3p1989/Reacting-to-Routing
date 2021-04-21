9aabe64aabafc528a3573a09903a74919d298550
L3p1989 <phogan110@live.com>
2021-04-21 00:43:40 -0700

    set up FilmsCard

---
+++ src/components/FilmsCard.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+const FilmsCard = props => {
+    return (
+        <div className="col-6">
+            <div className="card shadow my-2">
+                <div className="card-body text-center">
+                    <h5 className="card-title"><u>{props.title}</u></h5>
+                    <p className="card-text">{props.description}</p>
+                    {props.rt_score > 70 ? <footer className="blockquote-footer text-success">{props.rt_score}</footer> : <footer className="blockquote-footer text-danger">{props.rt_score}</footer>}
+                    <Link to={`/films/${props.id}`} className="btn btn-primary">More Info</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FilmsCard;
