9ff174165f95b9e0c4c228f609411486e8dd8569
L3p1989 <phogan110@live.com>
2021-04-21 00:04:10 -0700

    imported Link and set up links to pages

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const NavBar = () => {
     return (
         <>
             <nav className="navbar navbar-light sticky-top" style={{ backgroundColor: "#e3f2fd" }}>
                 <img src="https://ghibliapi.herokuapp.com/images/logo.svg" alt="" className="navbar-brand" style={{ height: "3em" }} />
                 <ul className="navbar mr-auto">
-                    <div className="nav-link">Home</div>
-                    <div className="nav-link">Films</div>
-                    <div className="nav-link">People</div>
+                    <Link to="/" className="nav-link">Home</Link>
+                    <Link to="/Films" className="nav-link">Films</Link>
+                    <Link to="/People" className="nav-link">People</Link>
                 </ul>
             </nav>
         </>
