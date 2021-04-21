bea6be3ed1c5eca1b2afd4cff03b03c63162e4c9
L3p1989 <phogan110@live.com>
2021-04-21 00:57:28 -0700

    gave Home style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,8 @@
 const Home = () => {
     return (
-        <div className="container">
-            <div className="row justify-content-center mt-2">
-                <h1>Reacting to Studio Ghibli API</h1>
-            </div>
-            <div className="row justify-content-center mt-5">
-                <p className="font-weight-bold">The Studio Ghibli API catalogs the people, places, and things found in the worlds of Ghibli. It was created to help users discover resources, consume them via HTTP requests, and interact with them in whatever way makes sense. Navigation can be found on the top navbar.</p>
-            </div>
+        <div className="jumbotron text-center bg-light">
+            <h1 className="display-4">Reacting to Studio Ghibli API</h1>
+            <p className="lead">The Studio Ghibli API catalogs the people, places, and things found in the worlds of Ghibli. It was created to help users discover resources, consume them via HTTP requests, and interact with them in whatever way makes sense. Navigation can be found on the top navbar.</p>
         </div>
     );
 };
